dfdbe801297c1c5e95139aac6016a5c751c30167
肖 <603331487@qq.com>
2020-10-17 21:34:26 +0800

    修改返回数据类型为Boolean

--- xyhToast/toast.vue
+++ xyhToast/toast.vue
@@ -1,32 +1,45 @@
 <template>
   <div>
-    <div v-show="isShowDialog" id="xyhDialog">
+    <div v-show="isShowDialog"
+         id="xyhDialog"
+    >
       <div id="xyhDialogInfo">
-        <div id="iconBg" :style="imgBgStyle">
-          <img id="stateIcon" :class="animatCss" :src="imgIcon" />
+        <div id="iconBg"
+             :style="imgBgStyle"
+        >
+          <img id="stateIcon"
+               :class="animatCss"
+               :src="imgIcon"
+          />
         </div>
         <div id="xyhDialogTitle">{{ dialogTitle }}</div>
         <div id="xyhDialogMsg">{{ dialogMsg }}</div>
-        <div v-show="showDialogBtn" id="xyhDialogInfoBtnGroup">
-          <div
-            v-show="showCancel"
-            id="xyhDialogInfoBtnClose"
-            class="btnGroupItems"
-            data-state="false"
-            cancel
-            @click="dialogBtnClick"
-          >{{ dialogCancelInfo }}</div>
-          <div
-            id="xyhDialogInfoBtn"
-            class="btnGroupItems"
-            data-state="true"
-            :style="{ backgroundColor: btnColor }"
-            @click="dialogBtnClick"
-          >{{ dialogPassInfo }}</div>
+        <div v-show="showDialogBtn"
+             id="xyhDialogInfoBtnGroup"
+        >
+          <div v-show="showCancel"
+               id="xyhDialogInfoBtnClose"
+               class="btnGroupItems"
+               data-state="false"
+               cancel
+               @click="dialogBtnClick"
+          >
+            {{ dialogCancelInfo }}
+          </div>
+          <div id="xyhDialogInfoBtn"
+               class="btnGroupItems"
+               data-state="true"
+               :style="{ backgroundColor: btnColor }"
+               @click="dialogBtnClick"
+          >
+            {{ dialogPassInfo }}
+          </div>
         </div>
       </div>
     </div>
-    <div v-show="isShowLoading" id="xyhLoading">
+    <div v-show="isShowLoading"
+         id="xyhLoading"
+    >
       <div id="xyhLoadingInfo">
         <img src="./img/loadingBg.png" />
         <div id="xyhLoadingInfoContent">
@@ -39,54 +52,54 @@
 </template>
 
 <script>
-import infoBg from "./img/infoBg.png";
-import infoIcon from "./img/info.png";
-import successBg from "./img/successBg.png";
-import successIcon from "./img/success.png";
-import errorBg from "./img/errorBg.png";
-import errorIcon from "./img/error.png";
+import infoBg from './img/infoBg.png';
+import infoIcon from './img/info.png';
+import successBg from './img/successBg.png';
+import successIcon from './img/success.png';
+import errorBg from './img/errorBg.png';
+import errorIcon from './img/error.png';
 
 export default {
-  name: "XyhDialog",
+  name: 'XyhDialog',
   data() {
     return {
       isShowDialog: false,
       isShowLoading: false,
-      loadingMsg: "",
+      loadingMsg: '',
       showDialogBtn: false,
       showCancel: false,
-      btnColor: "#234173",
+      btnColor: '#234173',
       imgBgStyle: {
-        backgroundImage: "url(" + infoBg + ")",
+        backgroundImage: 'url(' + infoBg + ')',
       },
       imgIcon: infoIcon,
-      animatCss: ["vivify"],
+      animatCss: ['vivify'],
       typeArr: [
         {
           icon: infoIcon,
           iconBg: infoBg,
-          cls: "swoopInTop",
+          cls: 'swoopInTop',
         },
         {
           icon: successIcon,
           iconBg: successBg,
-          cls: "flipInY",
+          cls: 'flipInY',
         },
         {
           icon: errorIcon,
           iconBg: errorBg,
-          cls: "jumpInRight",
+          cls: 'jumpInRight',
         },
       ],
-      dialogTitle: "",
-      dialogMsg: "",
-      dialogCancelInfo: "",
-      dialogPassInfo: "",
+      dialogTitle: '',
+      dialogMsg: '',
+      dialogCancelInfo: '',
+      dialogPassInfo: '',
     };
   },
   methods: {
     showLoading(msg) {
-      msg = msg || "正在加载中";
+      msg = msg || '正在加载中';
       this.isShowLoading = true;
       this.loadingMsg = msg;
     },
@@ -94,10 +107,10 @@ export default {
       this.isShowLoading = false;
     },
     showDialog({
-      title = "",
-      msg = "",
+      title = '',
+      msg = '',
       duration = 0,
-      button = "确定",
+      button = '确定',
       close = true,
       type = 0,
       callback = null,
@@ -112,8 +125,8 @@ export default {
         // 判断传入的close为boolean还是string
         if (close || close.length > 0) {
           this.showCancel = true;
-          typeof close === "boolean"
-            ? (this.dialogCancelInfo = "取消")
+          typeof close === 'boolean'
+            ? (this.dialogCancelInfo = '取消')
             : (this.dialogCancelInfo = close);
         }
       }
@@ -122,22 +135,21 @@ export default {
         ? this.typeArr[type]
         : this.typeArr[0];
       // 选择class样式
-      this.animatCss = ["vivify", typeInfo.cls];
-      this.imgBgStyle = { backgroundImage: "url(" + typeInfo.iconBg + ")" };
+      this.animatCss = ['vivify', typeInfo.cls];
+      this.imgBgStyle = { backgroundImage: 'url(' + typeInfo.iconBg + ')' };
       this.imgIcon = typeInfo.icon;
       this.isShowDialog = true;
       this.dialogCallBack = callback;
     },
     hideDialog() {
       this.isShowDialog = false;
-      this.animatCss = ["vivify"];
+      this.animatCss = ['vivify'];
     },
     dialogBtnClick(e) {
       const state = e.target.dataset.state;
-      this.dialogCallBack(state === "true");
+      this.dialogCallBack(state === 'true');
       this.hideDialog();
     },
-    dialogCallBack() {},
   },
 };
 </script>
